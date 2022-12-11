faac48790aa833db7ac03ad91667ee8d2c17e209
mrosesyntax <mrose@syntaxsdg.com>
2022-12-11 15:03:27 -0600

    Split run_game into helper methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,17 +23,25 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-        # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            #Redraw background color
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            #Make most recently drawn scree visible
+            self._check_events()
+            self._update_screen()
+        
+            #Make most recently drawn screen visible
             pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to key presses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        #Redraw background color, draw ship
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #Make most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ ==  '__main__':
     # Make a game instance and run the game
