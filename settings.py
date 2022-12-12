652bb5fe45914e01037ecf6efaf28326b15f2362
mrosesyntax <mrose@syntaxsdg.com>
2022-12-12 07:53:58 -0600

    Boundaries on ship movement, q to quit, refactored key checks

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        self.ship_speed = 21.5
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
         """Initialize the ship and starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship and get is rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -13,6 +14,8 @@ class Ship:
 
         # Start ship at bottom center of screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
 
         self.moving_right = False
         self.moving_left = False
@@ -22,7 +25,13 @@ class Ship:
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        if self.moving_right == True:
-            self.rect.x += 3
-        if self.moving_left == True:
-            self.rect.x -= 3
+        """Update the position of the ship if moving"""
+        # Note: Need to check if ship will leave screen IF move is applied
+        # if self.rect.right + self.settings.ship_speed < self.screen_rect.right
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        #Set ships rect pos from self.x
+        self.rect.x = self.x
